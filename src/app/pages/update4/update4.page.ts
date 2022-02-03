bd77760e4a28942ebb7c70b2a46fd3a75a13165a
shirisha <samudrala.shirisha413@gmail.com>
2022-02-03 19:31:13 +0530

    ds update

--- src/app/pages/update4/update4.page.ts
+++ src/app/pages/update4/update4.page.ts
@@ -32,9 +32,12 @@ aarveRepresName: any;
 subAgencyRepresentivaeName: any;
 clientRepresNaame: any;
 layer1List: any = [];
-  constructor(public toastSer: ToastService,
+
+constructor(public toastSer: ToastService,
     public androidDatabase: AndroidDatabaseService,
-    public router: Router) { }
+    public router: Router) {
+      this.getLayer1();
+     }
 
   ngAfterViewInit() {
     this.signaturePad = new SignaturePad(this.canvasEl.nativeElement);
@@ -70,4 +73,61 @@ layer1List: any = [];
 
 
   }
+  getLayer1() {
+    this.androidDatabase.getLayer1ById(Constants.laYer1Id).then((data) => {
+      this.layer1List = [];
+      console.log('size',data.rows.length);
+      if (data.rows.length > 0) {
+        for (let i = 0; i < data.rows.length; i++) {
+          this.layer1List.push(data.rows.item(i));
+        }
+        console.log('layer1List',this.layer1List);
+        if(this.layer1List.length>0){
+
+         this.depthOfTermination = this.layer1List[0].depth_termination;
+         this.date = this.layer1List[0].bh_enddate;
+         this.aarveRepresName = this.layer1List[0].rv_rep_name;
+         this.base641 = this.layer1List[0].rv_rep_sign;
+         this.subAgencyRepresentivaeName = this.layer1List[0].sa_rep_name;
+         this.base642 = this.layer1List[0].sa_rep_sign;
+         this.clientRepresNaame = this.layer1List[0].client_rep_name;
+         this.base643 = this.layer1List[0].client_rep_sign;
+         this.signaturePad.fromDataURL(this.base641);
+         this.signaturePad1.fromDataURL(this.base642);
+         this.signaturePad2.fromDataURL(this.base643);
+
+
+
+        }
+
+      }
+    });
+  }
+
+  updateImg1(){
+this.signaturePad.clear();
+  }
+  updateImg2(){
+    this.signaturePad1.clear();
+
+  }
+  updateImg3(){
+    this.signaturePad2.clear();
+
+  }
+  addDatabase(){
+    this.base641 = this.signaturePad.toDataURL();
+    this.signatureImg = this.base641;
+    this.base642 = this.signaturePad1.toDataURL();
+    this.signatureImg1 = this.base642;
+    this.base643 = this.signaturePad2.toDataURL();
+    this.signatureImg2 = this.base643;
+console.log(this.base641,this.base642,this.base643);
+    this.androidDatabase.updateLayer4(this.depthOfTermination,this.date,
+      this.aarveRepresName,this.base641,this.subAgencyRepresentivaeName,this.base642,this.clientRepresNaame,this.base643,
+      Constants.laYer1Id);
+      this.router.navigate(['viewlist']);
+  }
+
+
 }
